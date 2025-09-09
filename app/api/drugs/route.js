c38e0355b4c5b0de827cd146b1bdd188bf633939
Nikodem <wnikodem04@gmail.com>
2025-09-09 21:33:15 +0200

    implement drug search and initial info sidebar

---
+++ app/api/drugs/route.js
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+
+const client = await MongoClient.connect(process.env.MONGODB_URI);
+
+export async function GET() {
+	try {
+		const db = client.db("clinicDB");
+		const drugs = await db.collection("drugs").find().toArray();
+
+		return new Response(JSON.stringify(drugs), {
+			status: 200,
+			headers: { "Content-Type": "application/json" },
+		});
+	} catch (error) {
+		return new Response(JSON.stringify({ error: "Błąd połączenia z bazą" }), {
+			status: 500,
+			headers: { "Content-Type": "application/json" },
+		});
+	}
+}
--- app/components/MedicationsList.js
+++ app/components/MedicationsList.js
@@ -1,6 +1,7 @@
 "use client";
 
 import styles from "./MedicationsList.module.css";
+import { Info } from "lucide-react";
 import {
 	forwardRef,
 	useRef,
@@ -11,10 +12,30 @@ import {
 import { createPortal } from "react-dom";
 
 const Modal = forwardRef(function MedicationsModal({}, ref) {
+	const [selectedDrug, setSelectedDrug] = useState(null);
+	const [drugs, setDrugs] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [findDrug, setFindDrug] = useState("");
+
+	useEffect(() => {
+		async function fetchDrugs() {
+			const res = await fetch("/api/drugs");
+			const data = await res.json();
+			console.log("Fetched drugs:", data);
+			setLoading(false);
+			setDrugs(data);
+		}
+		fetchDrugs();
+	}, []);
+
+	const results = drugs.filter((d) =>
+		d.productName.toLowerCase().includes(findDrug.toLowerCase())
+	);
+
 	const [isOpen, setIsOpen] = useState(false);
-	const medications = ["Lek 1", "Lek 2", "Lek 3", "Lek 4"];
 
 	const dialog = useRef();
+	
 	useImperativeHandle(ref, () => {
 		return {
 			open: () => setIsOpen(true),
@@ -37,28 +58,68 @@ const Modal = forwardRef(function MedicationsModal({}, ref) {
 	if (!isOpen) return null;
 
 	return createPortal(
-		<dialog
-			ref={dialog}
-			className={styles.dialogBackdrop}
-			onClick={handleClose}
-		>
+		<dialog ref={dialog} className={styles.dialogBackdrop}>
 			<div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
 				<div className={styles.header}>
-					<h2>Twoje leki</h2>
+					<h2>Leki</h2>
 					<button className={styles.closeBtn} onClick={handleClose}>
 						&times;
 					</button>
 				</div>
 
-				<div className={styles.content}>
-					{medications.map((med) => (
-						<div className={styles.medicationItem} key={med}>
-							<p>{med}</p>
-							<button className={styles.addBtn}>Dodaj</button>
-						</div>
-					))}
-				</div>
+				<input
+					type="text"
+					placeholder="Wyszukaj lek..."
+					value={findDrug}
+					onChange={(e) => setFindDrug(e.target.value)}
+				></input>
+
+				{loading ? (
+					<p>Ładowanie leków...</p>
+				) : (
+					<div className={styles.content}>
+						{results.map((drug) => (
+							<div className={styles.medicationItem} key={drug._id}>
+								<p>{drug.productName}</p>
+								<div className={styles.buttons}>
+									<button className={styles.addBtn}>Dodaj</button>
+									<button
+										className={styles.infoBtn}
+										onClick={() => setSelectedDrug(drug)}
+									>
+										<Info size={18} />
+									</button>
+								</div>
+							</div>
+						))}
+					</div>
+				)}
 			</div>
+
+			{selectedDrug && (
+				<div className={styles.detailsPanel}>
+					<div className={styles.detailsHeader}>
+						<h3>{selectedDrug.productName}</h3>
+						<button
+							onClick={() => setSelectedDrug(null)}
+							className={styles.closeBtn}
+						>
+							&times;
+						</button>
+					</div>
+					<div className={styles.detailsContent}>
+						<p>
+							<strong>Moc:</strong> {selectedDrug.strength}
+						</p>
+						<p>
+							<strong>Rodzaj preparatu:</strong> {selectedDrug.preparationType}
+						</p>
+						<p>
+							<strong>Kod ATC:</strong> {selectedDrug.atcCode}
+						</p>
+					</div>
+				</div>
+			)}
 		</dialog>,
 		document.body
 	);
