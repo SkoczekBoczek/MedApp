1c94e35049fe6dc6992d4a1e24a648e2afa73f4b
Nikodem <wnikodem04@gmail.com>
2026-01-10 19:09:40 +0100

    add DayCard component

--- app/components/MedicationsSection/MedicationsSection.js
+++ app/components/MedicationsSection/MedicationsSection.js
@@ -3,7 +3,8 @@ import styles from "./MedicationsSection.module.css";
 import MedicationsModal from "./MedicationsList";
 import userToken from "@/utils/userToken";
 import { useEffect, useRef, useState } from "react";
-import { Plus, Pill, Clock, PlusCircle, Trash, Syringe } from "lucide-react";
+import { PlusCircle } from "lucide-react";
+import DayCard from "./DayCard";
 
 export default function MedicationsSection() {
 	const [weeklyPlan, setWeeklyPlan] = useState([
@@ -131,86 +132,13 @@ export default function MedicationsSection() {
 				<div className={styles.weeklyPlan}>
 					{weeklyPlan.map((dayPlan) => {
 						return (
-							<div key={dayPlan.day} className={styles.dayCard}>
-								<div className={styles.dayHeader}>
-									<h3 className={styles.dayTitle}>{dayPlan.day}</h3>
-									<button
-										onClick={() => showModal(dayPlan.day)}
-										className={styles.addButton}
-										title="Dodaj lek"
-									>
-										<Plus size={16} />
-									</button>
-								</div>
-
-								<div className={styles.medicationsList}>
-									{dayPlan.medications.length === 0 ? (
-										<p className={styles.noMedications}>
-											Brak zaplanowanych leków
-										</p>
-									) : (
-										dayPlan.medications.map((med) => (
-											<div
-												key={med.instanceId}
-												className={`${styles.medicationItem} ${
-													med.taken ? styles.medicationTaken : ""
-												}`}
-												onClick={() => {
-													toggleMedicationTaken(dayPlan.day, med.instanceId);
-												}}
-											>
-												<div className={styles.medicationContent}>
-													<div className={styles.medicationInfo}>
-														{/* GODZINA */}
-														<div className={styles.timeRow}>
-															<Clock size={14} className={styles.icon} />
-															<span className={styles.time}>{med.time}</span>
-														</div>
-														{/* NAZWA */}
-														<div className={styles.nameRow}>
-															<Pill size={14} className={styles.icon} />
-															<span className={styles.name}>
-																{med.productName}
-															</span>
-														</div>
-														{/* DAWAKA */}
-														<div className={styles.doseRow}>
-															<Syringe size={14} className={styles.icon} />
-															<span className={styles.name}>{med.dose}</span>
-														</div>
-													</div>
-
-													<div className={styles.btnss}>
-														<div
-															className={`${styles.checkbox} ${
-																med.taken ? styles.checkboxChecked : ""
-															}`}
-														>
-															{med.taken && (
-																<div className={styles.checkboxInner}>
-																	<div className={styles.checkboxDot}></div>
-																</div>
-															)}
-														</div>
-														<button
-															className={styles.deleteBtn}
-															onClick={(e) => {
-																e.stopPropagation();
-																handleDeleteMedication(
-																	med.instanceId,
-																	dayPlan.day
-																);
-															}}
-														>
-															<Trash size={18} />
-														</button>
-													</div>
-												</div>
-											</div>
-										))
-									)}
-								</div>
-							</div>
+							<DayCard
+								key={dayPlan.day}
+								dayPlan={dayPlan}
+								showModal={showModal}
+								onDeleteMedication={handleDeleteMedication}
+								toggleMedicationTaken={toggleMedicationTaken}
+							/>
 						);
 					})}
 				</div>
